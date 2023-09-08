05a987866d0d0b3f825bc5792f641c6f0eeb39a0
Melanie-J-Baker <baker_mel@hotmail.com>
2023-09-08 16:22:54 +0100

    Add margin to contact link @media

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const html = document.getElementById("html");
 const home = document.querySelector(".home-icon");
 const menuFern = document.querySelector(".menu-fern");
 const endFern = document.querySelector(".fern");
+const profile = document.querySelector(".profile-photo");
 slider.addEventListener("click", () => {
   if (html.classList.contains("dark")) {
     html.classList.remove("dark");
@@ -10,11 +11,13 @@ slider.addEventListener("click", () => {
     home.src = "assets/home-black.svg";
     menuFern.src = "assets/fern_black.svg";
     endFern.src = "assets/fern_black.svg";
+    profile.src = "assets/user_black.svg";
   } else {
     html.classList.remove("light");
     html.classList.add("dark");
     home.src = "assets/home.svg";
     menuFern.src = "assets/fern_white.svg";
     endFern.src = "assets/fern_white.svg";
+    profile.src = "assets/user.svg";
   }
 });
